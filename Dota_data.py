5f6d55d084b909f3fcb7c8e065c948b30c7c7553
Lunkers <freddelunk@gmail.com>
2017-03-08 20:06:31 +0100

    Rough start, will keep working on it

---
+++ Dota_data.py
@@ -0,0 +1,4 @@
+heroes_dict = {1:"Anti-mage", 2:"Axe", 3:"Bane", 4: "Bloodseeker", 5:"Crystal Maiden", 6: "Drow Ranger"}
+
+"""for key in heroes_dict:
+   print(key, heroes_dict[key])"""
---
+++ Grapher.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import pandas
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import matplotlib
+import Dota_data
+
+matplotlib.style.use('ggplot')
+
+player_id = input("Enter player ID: ")
+
+def get_heroes_data(playerId):
+    url = "https://api.opendota.com/api/players/"+ playerId+ "/heroes"
+    header = {'x-requested with': "XMLHttpRequest"}
+    data = requests.get(url, header)
+    df = pandas.read_json(url)
+    for key in Dota_data.heroes_dict:
+        df = df.replace(to_replace={"hero_id":{key:Dota_data.heroes_dict[key]}})
+    return df
+
+heroes_meme = get_heroes_data(player_id)
+print(heroes_meme)
+
+
+def graph_heroes_wins(data):
+    data.plot(x="hero_id", y="win", kind="bar" )
+    plt.show()
+
+graph_heroes_wins(heroes_meme)
+
